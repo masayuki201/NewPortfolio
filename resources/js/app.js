f3dac5348ce16ae615c7d4d8a0dd60e13f719975
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-06-03 07:05:18 +0900

    add:vueを使用できるように、追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import './bootstrap'
+import Vue from 'vue'
+import ExampleComponent from "./components/ExampleComponent";
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        ExampleComponent,
+    }
+});
