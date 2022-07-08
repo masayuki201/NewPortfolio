862865992d72fe847e72f00792ec3bac069a9de9
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-07-09 06:34:51 +0900

    add:Create logic for Ranking Page

---
+++ app/Http/Controllers/RankingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\RankingServiceInterface;
+
+class RankingController extends Controller
+{
+    protected $service;
+
+    public function __construct(RankingServiceInterface $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        $videoRanking = $this->service->getRankingPage($request);
+
+        return view('ranking.index', $videoRanking);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use App\Services\ListService;
 use App\Services\ListServiceInterface;
+use App\Services\PickupService;
+use App\Services\PickupServiceInterface;
+use App\Services\RankingService;
+use App\Services\RankingServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,7 +20,13 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(
-            ListServiceInterface::class, ListService::class
+            ListServiceInterface::class, ListService::class,
+        );
+        $this->app->bind(
+            PickupServiceInterface::class, PickupService::class,
+        );
+        $this->app->bind(
+            RankingServiceInterface::class, RankingService::class,
         );
     }
 
