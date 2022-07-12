41bc107fbd47e30d69f6831a521b55227e9778c8
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-07-13 06:41:59 +0900

    add:Create Register Page

---
+++ resources/lang/ja/register.php
@@ -0,0 +1,19 @@
+<?php
+return [
+
+    'title' => 'ユーザー登録',
+    'google_button' => 'Googleで登録',
+    'nickname' => 'ニックネーム',
+    'email' => 'メールアドレス',
+    'password' => 'パスワード',
+    'password_verification' => 'パスワード（確認）',
+    'submit'  => '登録',
+
+
+
+
+
+    'nickname.attention' => '英数字3〜16文字（登録後の変更はできません）',
+    'login.attention' => 'ログインはこちら',
+
+];
