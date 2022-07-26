1a0b7d5d7015c3ca483d61fab829c2f185baedca
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-07-27 05:06:19 +0900

    add:Create logic for ResetPassword

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Mail\BareMail;
+use App\Notifications\PasswordResetNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -32,6 +34,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new PasswordResetNotification($token, new BareMail()));
+    }
 
     public $timestamps = false;
 }
