15c7928de3750cde33c0e450f530fdb975310a35
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-07 17:31:40 +0900

    refactor

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\ListServiceInterface;
-use App\Services\ListService;
 use Illuminate\Http\Request;
-use App\Models\Video;
-use App\Models\Target;
+
 
 
 class ListController extends Controller
