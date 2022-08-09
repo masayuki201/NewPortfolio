3cd00e0778f68fcc85097e6d5fc4f3cf00bf8c4a
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-10 05:20:34 +0900

    add:Create logic for submit Videos

--- app/Services/VideosServiceInterface.php
+++ app/Services/VideosServiceInterface.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace App\Services;
+
+use http\Env\Request;
+use App\Http\Requests\VideosRequest;
+
+interface VideosServiceInterface
+{
+    public function getCreatePage($request);
+
+    public function storeVideos(VideosRequest $request);
+
+    public function destroyVideos($id);
+}
