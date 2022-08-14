d77a6edbdd5099f95e8219786e2e2cf4bfcede8d
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-14 09:23:37 +0900

    add:Create page language for GoogleID login/register

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -3,22 +3,26 @@
 return [
 
     'login' => 'ログイン',
+    'login_google' =>'Googleでログイン',
     'register' => '新規登録',
     'email' => 'メールアドレス',
     'password' => 'パスワード',
     'nickname' => 'ニックネーム',
+    'id' => 'ユーザID',
 
     'submit'  => '登録',
+    'delete' => '削除',
 
 
     'goto_google' => 'Googleで登録',
     'goto_login' => 'ログインはこちら',
     'goto_register' => 'ユーザー登録はこちら',
     'password_reset' => 'パスワード再設定',
+    'password_lost' => 'パスワードを忘れた方',
     'send_email' => 'メール送信',
     'password_verification' => 'パスワード（確認）',
 
-    'nickname.attention' => '英数字3〜16文字（登録後の変更はできません）',
+    'nickname.attention' => '3〜16文字（登録後の変更はできません）',
 
 
     'failed' => 'ログイン情報が登録されていません。',
