9bb251f2f91dcef736f1be0b5596246b50dec093
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-16 05:21:23 +0900

    add:Create language for My Page show/edit/update/delete

---
+++ resources/lang/ja/users.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'title' => 'マイページ',
+    'title2' => 'マイページ編集',
+
+    'email' => 'メールアドレス',
+    'nickname' => 'ニックネーム',
+    'id' => 'ユーザID',
+
+    'submit'  => '登録',
+    'edit' => '編集',
+    'remove' => '退会',
+   'edit_remove' => '編集 / 退会'
+
+];
