22695db0a2c4e05a882a3e53b32e1dfd74f0b2ed
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-20 18:56:02 +0900

    Update: Database for Users table

--- database/migrations/2022_06_02_035726_create_users_table.php
+++ database/migrations/2022_06_02_035726_create_users_table.php
@@ -14,14 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id')->comment('ユーザーID');
+            $table->Bigincrements('id')->comment('ユーザーID');
             $table->string('nickname')->unique()->comment('ニックネーム');
             $table->string('email')->unique()->comment('メールアドレス');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable()->comment('パスワード');
             $table->char('delete_flag',1)->default(0)->comment('deleteフラグ');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
