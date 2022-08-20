7eeda99f0dc0e0985e82c34e2d9b3bb73017c8bc
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-20 18:55:40 +0900

    Update: Database for Videos table

--- database/migrations/2022_06_02_235749_create_videos_table.php
+++ database/migrations/2022_06_02_235749_create_videos_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 class CreateVideosTable extends Migration
 {
@@ -14,11 +16,12 @@ class CreateVideosTable extends Migration
     public function up()
     {
         Schema::create('videos', function (Blueprint $table) {
-            $table->increments('id')->comment('動画ID');
-            $table->integer('user_id')->unsigned()->index()->comment('ユーザーID');
+            $table->Bigincrements('id')->comment('動画ID');
+            $table->bigInteger('user_id')->unsigned()->index()->comment('ユーザーID');
             $table->string('url', 11)->comment('URL');
-            $table->integer('target_id')->unsigned()->comment('対象ID');
-            $table->timestamps();
+            $table->bigInteger('target_id')->unsigned()->comment('対象ID');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
 
             //ユーザーID外部キー制約
             $table->foreign('user_id')
