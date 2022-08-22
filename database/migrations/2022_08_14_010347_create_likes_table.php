b9bd3104f5dad26a17c46c87180b42427276fb9a
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-23 05:08:09 +0900

    add:Create Database for lliked tables

---
+++ database/migrations/2022_08_14_010347_create_likes_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('user_id')->unsigned()->comment('ユーザーID');
+            $table->bigInteger('video_id')->unsigned()->comment('動画ID');
+
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+
+            //ユーザーID外部キー制約
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            //動画ID外部キー制約
+            $table->foreign('video_id')
+                ->references('id')
+                ->on('videos')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
