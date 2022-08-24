315fe6d0b2fb45d129620beed592262c66aac739
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-25 06:22:38 +0900

    add: Create like function on page for index

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,19 @@ import NameChip from "./components/NameChip";
 import VideoCard from "./components/VideoCard";
 import TargetgradeChip from "./components/TargetgradeChip";
 import RankingChip from "./components/RankingChip";
+import VideoLike from "./components/VideoLike";
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
     components: {
         NameChip,
         VideoCard,
         TargetgradeChip,
         RankingChip,
+        VideoLike,
     }
 });
