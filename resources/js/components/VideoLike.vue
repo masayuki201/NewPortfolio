2ab2fee5daf7d3d44bc9b0364dcf654558b11eba
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-08-25 06:22:21 +0900

    add: Create like function on page for index

---
+++ resources/js/components/VideoLike.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <v-btn
+           icon
+           class="btn m-0 p-1 shadow-none"
+        >
+        <v-avatar>
+            <v-icon
+                @click="clickLike"
+                :color="likeColor()"
+                :class="gotoLiked()"
+            >
+                mdi-heart
+            </v-icon>
+        </v-avatar>
+        </v-btn>
+        {{ countLikes }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        initialIsLikedBy: {
+            type: Boolean,
+            default: false,
+        },
+        initialCountLikes: {
+            type: Number,
+            default: 0,
+        },
+        authorized: {
+            type: Boolean,
+            default: false,
+        },
+        endpoint: {
+            type: String,
+        },
+    },
+    data() {
+        return {
+            isLikedBy: this.initialIsLikedBy,
+            countLikes: this.initialCountLikes,
+        }
+    },
+    methods: {
+        likeColor() {
+            if(this.isLikedBy){
+                return  "#F81B1BFF";
+            }
+        },
+        gotoLiked() {
+            if(this.isLikedBy){
+                return "animated heartBeat fast";
+            }
+        },
+        clickLike() {
+            if (!this.authorized) {
+                alert('いいね機能はログイン中のみ使用できます')
+                return
+            }
+            this.isLikedBy ? this.unlike() : this.like()
+        },
+        async like() {
+            const response = await axios.put(this.endpoint)
+            this.isLikedBy = true
+            this.countLikes = response.data.countLikes
+        },
+        async unlike() {
+            const response = await axios.delete(this.endpoint)
+            this.isLikedBy = false
+            this.countLikes = response.data.countLikes
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
