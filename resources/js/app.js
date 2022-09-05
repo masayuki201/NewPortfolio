2c59ccf93d76b83b3856cdbb5767d3ca5fc43884
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-09-06 07:01:32 +0900

    add:Create Follow button

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import './bootstrap'
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import NameChip from "./components/NameChip";
 import VideoCard from "./components/VideoCard";
 import TargetgradeChip from "./components/TargetgradeChip";
 import RankingChip from "./components/RankingChip";
 import VideoLike from "./components/VideoLike";
-import Vuetify from 'vuetify'
+import FollowButton from "./components/FollowButton";
 
 Vue.use(Vuetify);
 
@@ -18,5 +19,6 @@ const app = new Vue({
         TargetgradeChip,
         RankingChip,
         VideoLike,
+        FollowButton,
     }
 });
