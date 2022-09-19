2160c732a82642cd66b138c50b9a7206e4b3b7a1
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-09-20 07:11:53 +0900

    add:Create a page for the follow list/follower list

---
+++ resources/js/components/PersonNameChip.vue
@@ -0,0 +1,39 @@
+<template>
+    <a :href="`/list/${nickname}/detail`" class="text-white">
+        <v-chip
+            class="ma-2"
+            color="orange"
+            text-color="white"
+            x-large
+        >
+            <a :href="`/list/${nickname}/detail`" class="text-white">
+                <v-avatar left>
+                    <v-icon
+                        x-large
+                    >
+                        mdi-account-circle
+                    </v-icon>
+                </v-avatar>
+                {{ nickname }}
+            </a>
+        </v-chip>
+    </a>
+</template>
+
+<script>
+export default {
+    name: "NameChip",
+    data() {
+        return {
+            nickname: this.nickname,
+        }
+    },
+    props: {
+        nickname: {
+            type: String,
+        }
+    }
+}
+
+</script>
+
